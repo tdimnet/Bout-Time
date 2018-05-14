9ce629c2b4b8263db07894a5d7389360858cf63a
tdimnet <thomas.dimnet@gmail.com>
2018-05-14 17:27:09 +0200

    Initial Commit

---
+++ Bout Time/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bout Time
+//
+//  Created by Thomas Dimnet on 14/05/2018.
+//  Copyright © 2018 Thomas Dimnet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
